6c165cc652d2c31ad89478df26cff7e894b1fb15
Sven Borden <glaglasven@live.com>
2022-07-30 21:57:03 +0200

    Simplified version

--- ah-brogniard/replacetxt.js
+++ ah-brogniard/replacetxt.js
@@ -1,36 +1,8 @@
+// replacing all the elements with the word "lol"
 
-function shoutAh() {
-    console.log("ahhhhh");
-    alert('the');
-}
-
-function walk(node) {
-
-  var child, next;
-  switch (node.nodeType) {
-
-    case 1:
-      handleText(node);
-      break;
-    case 9:
-    case 11:
-      child = node.firstChild;
-      while (child) {
-        next = child.nextSibling;
-        walk(child);
-        child = next;
-      }
-      break;
-
-    case 3:
-      break;
-  }
-}
-
-function handleText(node) {
-  var v = node.innerHTML;
-  v = v.replace(/the/gi, '<span onmouseover="alert("the")">$&</span>');
-  node.innerHTML = v;
-}
-
-walk(document.body);
+let elements = Array.prototype.slice.call(
+  document.querySelectorAll('p, h1, h2, h3, h4, h5, span, i')
+);
+elements.forEach(function(ele) {
+  ele.textContent = ele.textContent.replace(/the/g, 'lol');
+});
